3303400c34a00beb363049ff3fc4934038c8ed8e
Volkov <fiveeyesfiveyears@gmail.com>
2023-02-24 12:02:37 +0300

    #2. Loading structures.

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -2,3 +2,15 @@
 #define C___14__MATRICES_MATRIX_H
 
 #endif //C___14__MATRICES_MATRIX_H
+
+typedef struct matrix {
+    int **values; // элементы матрицы
+    int nRows; // количество рядов
+    int nCols; // количество столбцов
+} matrix;
+
+typedef struct position {
+    int rowIndex;
+    int colIndex;
+} position;
+
