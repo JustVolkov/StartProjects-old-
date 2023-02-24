3442bd24ab0009ab26a5b57581c5dde646e716b8
Volkov <fiveeyesfiveyears@gmail.com>
2023-02-24 12:57:53 +0300

    #11. Добавление функции countZeroRows(...), вспомогательной функции createArrayOfMatrixFromArray(...) для её тестирования и самой тестирующей функции.

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -298,3 +298,45 @@ Position getMaxValuePos(Matrix m) {
     ResultPosition.colIndex = i;
     return ResultPosition;
 }
+
+//------------
+
+// Возвращает матрицу размера nRows на nCols, построенную из элементов массива a:
+Matrix* createArrayOfMatrixFromArray(const int* values, size_t nMatrices, size_t nRows, size_t nCols) {
+    Matrix* ms = getMemArrayOfMatrices(nMatrices, nRows, nCols);
+    int l = 0;
+        for (size_t k = 0; k < nMatrices; k++)
+            for (size_t i = 0; i < nRows; i++)
+                for (size_t j = 0; j < nCols; j++)
+                    ms[k].values[i][j] = values[l++];
+    return ms;
+}
+
+// Один из возможных вариантов функции countZeroRows, впоследствии он будет протестирован:
+int countZeroRows(Matrix m) {
+    int sumOfRow = 0;
+    int numberOfZeroRows = 0;
+    for (int i = 0; i < m.nRows; i++) {
+        for (int l = 0; l < m.nCols; l++) {
+            sumOfRow += m.values[i][l];
+        }
+        if (sumOfRow == 0)
+            numberOfZeroRows++;
+        sumOfRow = 0;
+    }
+    return numberOfZeroRows;
+}
+
+// Тестирование функции countZeroRows:
+void test_countZeroRows() {
+    Matrix m = createMatrixFromArray
+            ((int[]) {
+                    1, 1, 0,
+                    0, 0, 0,
+                    0, 0, 1,
+                    0, 0, 0,
+                    0, 1, 1,
+                     }, 5, 3);
+    assert(countZeroRows(m) == 2);
+    freeMemMatrix(m);
+}
