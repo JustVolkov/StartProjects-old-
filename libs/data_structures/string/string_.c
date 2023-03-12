e0473cb65273989bb5d712a016379bfa5b733e97
Volkov <fiveeyesfiveyears@gmail.com>
2023-03-13 02:03:13 +0300

    #17.2, Добавлена функция find(...), возвращающая указатель на первый элемент с данным кодом (+ доп. особенности), а также функция для её тестирования.

--- libs/data_structures/string/string_.c
+++ libs/data_structures/string/string_.c
@@ -10,6 +10,7 @@ size_t strlen(char* begin) {
     return end - begin;
 }
 
+// Тестирование функции strlen.
 void strlen_test(int(strlen)(char* begin)) {
     char s1[] = "ABCDEF";
     char s2[] = "CAT";
@@ -19,4 +20,30 @@ void strlen_test(int(strlen)(char* begin)) {
     assert(strlen(s2) == 3);
     assert(strlen(s3) == 1);
     assert(strlen(s4) == 0);
+}
+
+//----------
+
+/* Возвращает указатель на первый элемент с кодом ch, расположенный
+(на ленте памяти) между адресами begin и end (не включая end);
+если символ не найден, возвращает значение end.
+ */
+char* find(char* begin, char* end, int ch) {
+    while (begin != end && *begin != ch)
+        begin++;
+
+    return begin;
+}
+
+// Тестирование функции find.
+void find_test(char*(find)(char* begin, char* end, int ch)) {
+    char s[] = "ISDAAAT";
+    char* p_begin = &s[0];
+    char* p_end = &s[strlen(s)];
+    // Числовые коды и соответствующие им символы (в ASCII):
+    int ch1 = 65;	// A
+    int ch2 = 73;	// I
+    int ch3 = 61;	// =
+    assert(find(p_begin, p_end, ch1) == &s[3]);
+    assert(find(p_begin, p_end, ch2) == &s[0]);
 }
